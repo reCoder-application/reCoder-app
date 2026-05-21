03fd17b10870d2e2463de99b154a5600dbe66919
nagisa-oda <andisan3434@gmail.com>
2026-05-21 12:46:23 +0900

    サーバ通信用のapi.jsを作成。必要関数の定義のみ行った

---
+++ js/api.js
@@ -0,0 +1,25 @@
+function syncStorage() {
+    localStorage.setItem('coffeeLogs', JSON.stringify(coffeeLogs));
+};
+
+// async: 処理に時間を要する関数に用いる
+async function loadLogs() {
+    
+};
+
+async function saveLog(log) {
+    
+};
+
+async function updateLog(Log) {
+    
+};
+
+async function deleteLog(Log) {
+    
+};
+
+// お気に入りを切り替える
+async function toggleFavorite(id, isFavorite) {
+    
+};
