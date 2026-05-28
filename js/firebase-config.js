7b8412dfce867935cf112985ad7e8bffb751de37
nagisa-oda <andisan3434@gmail.com>
2026-05-28 23:52:01 +0900

    firebase-config.jsを作成し、firebase上でもデータベースを本番モードで作成した

---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkli9fpKnERt8IQwte-2I7iafcCIlx4pM",
+  authDomain: "recoder-60bd7.firebaseapp.com",
+  projectId: "recoder-60bd7",
+  storageBucket: "recoder-60bd7.firebasestorage.app",
+  messagingSenderId: "1064599488965",
+  appId: "1:1064599488965:web:2d3dd56a6ea47c5fd0e3ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
