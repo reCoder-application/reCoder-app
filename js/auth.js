28532fa61ed76c5bc632463aa20d1546447379de
nagisa-oda <andisan3434@gmail.com>
2026-05-30 21:54:43 +0900

    認証用のauth.jsを新たに作成。現時点では変数の宣言のみ

---
+++ js/auth.js
@@ -0,0 +1,13 @@
+const authPage = document.getElementById('auth-page');
+const homePage = document.getElementById('home-page');
+const addBtn = document.getElementById('add-btn'); // 右下の追加ボタン
+const logoutBtn = document.getElementById('logout-btn')
+
+const authTitle = document.getElementById('auth-title');
+const authEmail = document.getElementById('auth-email');
+const authPassword = document.getElementById('auth-password');
+const authSubmitBtn = document.getElementById('auth-submit-btn');
+const authSwitchText = document.getElementById('auth-switch-text');
+const authSwitchLink = document.getElementById('auth-switch-link');
+const authError = document.getElementById('auth-error');
+
